b1565c97565e30e09343f2bc2150eb8ba9e4fae7
unknown <tupm@devcamp.edu.vn>
2025-01-15 17:39:56 +0700

    update FE,BE,ADMIN

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -8,13 +8,24 @@ const Navbar = ({ setToken }) => {
         <h1 className="text-4xl font-bold">Shop.</h1>
         <p className="text-pink-400 font-semibold">Admin Panel</p>
       </div>
-
-      <button
-        onClick={() => setToken("")}
-        className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm"
-      >
-        Đăng xuất
-      </button>
+      <div className="flex justify-center items-center gap-5">
+        <div>
+          <a
+            href="https://shop-frontend-sable.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="border rounded-full px-5 py-2 hover:bg-gray-300 border-black"
+          >
+            Vào trang Admin
+          </a>
+        </div>
+        <button
+          onClick={() => setToken("")}
+          className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm"
+        >
+          Đăng xuất
+        </button>
+      </div>
     </div>
   );
 };
